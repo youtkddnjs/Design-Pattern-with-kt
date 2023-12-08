6a64e64e9b26606ad82c17fd14d65ffdecc3ceee
youtkddnjs <youtkddnjs91@gmail.com>
2023-12-08 11:21:38 +0900

    Add Databinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    viewBinding{
-        enable = true
-    }
     dataBinding{
         enable = true
     }
@@ -47,6 +44,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/sw/sample/designpattern/MainActivity.kt
+++ app/src/main/java/sw/sample/designpattern/MainActivity.kt
@@ -2,10 +2,28 @@ package sw.sample.designpattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sw.sample.designpattern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+            this.view = this@MainActivity
+        }
+
+
+
+    }//override fun onCreate(savedInstanceState: Bundle?)
+
+    fun openMvc(){}
+    fun openMvp(){}
+    fun openMvvm(){}
+    fun openMvi(){}
+
+
+
+}//class MainActivity : AppCompatActivity()
